efe3f48f2103816cd130d3e0fe3fdfc3bbc8998a
captproton <carl@wdwhub.net>
2018-05-11 21:14:15 -0700

    INIT: user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :profiles
   devise_for :users
   mount RailsAdmin::Engine => '/backstage', as: 'rails_admin'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180511013516) do
+ActiveRecord::Schema.define(version: 20180512031429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "full_name"
+    t.string "mobile_phone"
+    t.string "biography"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
