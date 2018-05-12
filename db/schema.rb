9e3ad642d53873a3959d90e1f24283c62d14480b
captproton <carl@wdwhub.net>
2018-05-11 22:41:02 -0700

    ADDED: attributes to user profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180512031429) do
+ActiveRecord::Schema.define(version: 20180512052510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20180512031429) do
     t.string "biography"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "user_photo_url"
+    t.string "hourly_rate_in_usd"
   end
 
   create_table "users", force: :cascade do |t|
